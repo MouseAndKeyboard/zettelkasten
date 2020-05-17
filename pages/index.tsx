be49dc1a61c0ae2fce7d75a16c06cc4e5afe6a4b
M Nef <howtoeatpineapples@gmail.com>
2020-05-17 20:36:36 +0800

    Installed reactstrap

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,9 @@ import Layout from '../components/layout'
 export default function Home() {
   return (
       <Layout>
-          Home Page
+          <h1>Home Page</h1>
+
+
       </Layout>
   )
 }
