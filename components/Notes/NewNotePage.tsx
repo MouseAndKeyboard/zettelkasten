0b85c6df5a2ea439c197b5548bbe842869a48c38
M Nef <howtoeatpineapples@gmail.com>
2020-05-17 20:53:06 +0800

    Implemented form entry on notes page

--- components/Notes/NewNotePage.tsx
+++ components/Notes/NewNotePage.tsx
@@ -1,15 +1,46 @@
-import { Form, FormGroup, Label, Input, Button } from 'reactstrap'
+import { Form, FormGroup, Label, Input, Button, Spinner } from 'reactstrap'
+import { useState } from 'react'
 
 const NewNotePage = () => {
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState('')
+
+    const closeError = () => setError('')
+
+    const handleSubmit = async (values: {note: string, tags: string[]}) => {
+        setLoading(true)
+        const data = {
+            note: values.note,
+            tags: values.tags
+        }
+
+        try {
+            // post to api endpoint
+
+        } catch ({ code, message }) {
+            setError(message || 'Unexpected error')
+        } finally {
+            setLoading(false)
+        }
+
+    }
+
+    return (
     <div>
     <Form>
         <FormGroup>
             <Label for="noteArea">New Note</Label>
             <Input type="textarea" name="text" id="noteArea" />
         </FormGroup>
-        <Button>Submit</Button>
+        {
+            loading ?
+            <Button className="btn btn-primary" disabled>
+                <Spinner animation="grow" variant="success" /> Sending...
+            </Button> :
+            <Button className="btn btn-primary">Submit</Button>
+        }
     </Form>
-    </div>
+    </div>)
 }
 
 export default NewNotePage
