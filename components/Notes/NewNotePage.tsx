e68d85a77b16a214a776987fb9abbd10b33ab799
M Nef <howtoeatpineapples@gmail.com>
2020-05-18 10:39:51 +0800

    State for new note form input

--- components/Notes/NewNotePage.tsx
+++ components/Notes/NewNotePage.tsx
@@ -5,42 +5,56 @@ const NewNotePage = () => {
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState('')
 
+    const [formFields, setFormFields] = useState<NewNoteFormFields>({
+        note: '',
+        tags: []
+    })
+
     const closeError = () => setError('')
 
-    const handleSubmit = async (values: {note: string, tags: string[]}) => {
+    const handleSubmit = async () => {
         setLoading(true)
-        const data = {
-            note: values.note,
-            tags: values.tags
-        }
-
         try {
             // post to api endpoint
-
+            console.log(formFields)
         } catch ({ code, message }) {
             setError(message || 'Unexpected error')
         } finally {
             setLoading(false)
         }
-
     }
 
     return (
     <div>
-    <Form>
+    <Form onSubmit={handleSubmit}>
         <FormGroup>
             <Label for="noteArea">New Note</Label>
-            <Input type="textarea" name="text" id="noteArea" />
+            <Input
+                type="textarea"
+                name="text"
+                id="noteArea"
+                value={formFields.note}
+                onChange={e => setFormFields({
+                    note: e.target.value, tags: formFields.tags
+                })}
+            />
+
+
         </FormGroup>
         {
             loading ?
             <Button className="btn btn-primary" disabled>
                 <Spinner animation="grow" variant="success" /> Sending...
             </Button> :
-            <Button className="btn btn-primary">Submit</Button>
+            <Button className="btn btn-primary" onClick={() => handleSubmit()}>Submit</Button>
         }
     </Form>
     </div>)
+}
+
+interface NewNoteFormFields {
+    note: string,
+    tags: string[]
 }
 
 export default NewNotePage
